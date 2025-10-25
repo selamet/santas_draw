b38fd995299983402483cddf6ff9f5e1f659aa2c
selametsamli <selamet96@gmail.com>
2025-10-25 19:29:17 +0300

    Remove all files

--- apps/draw/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DrawConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.draw'
--- apps/user/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    team = models.CharField(max_length=255, blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
